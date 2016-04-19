7f0b80adf0201db96241bb99f7bd1b6f6fdf1ebc
jpavieira <vieirajoao94@gmail.om>
2016-04-19 20:19:05 +0100

    Player Interface added

---
+++ src/project/PlayerInterface.java
@@ -0,0 +1,30 @@
+package project;
+
+/** PlayerInterface interface
+ * Interface to the basic movements a BlackJack player can execute
+ * 
+ * @author Filipe Correia
+ * @author Hélder Duarte
+ * @author João Vieira
+ */
+public interface PlayerInterface {
+
+	/**
+	 * This method is used to ask for one card that will be randomly picked from shoe
+	 * 
+	 * @return card picked randomly
+	 * @see Hit
+	 */
+	public Card hit();
+	
+	/**
+	 * This method is used to keep the player's hand as it is.
+	 * 
+	 * @see Stand
+	 */
+	public void stand();
+	
+	
+	
+	
+}
